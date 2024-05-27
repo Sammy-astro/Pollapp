417fbccb0e878c0009302b690baa597459c2f63c
sammy <sammyastroblast@gmail.com>
2024-05-27 20:16:49 +0200

    Poll App

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+
+# It doesn't allow me to upload 2 files of the same name byt i did my assignment 
+# You can check it in my screen shots
+
+user_input = input("Please enter something: ")
+print(f"You entered: {user_input}")
